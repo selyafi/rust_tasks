5be0abd69c5ad3feccb027a5e03e02b18dca5ee2
Shadi Elyafi <elyafi@gmail.com>
2023-09-08 19:46:49 +0300

    code fixes

--- smart_socket/socket/src/main.rs
+++ smart_socket/socket/src/main.rs
@@ -1,38 +1,39 @@
-use std::io::{Read, Write};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 use socket_lib::{Command, Response};
 fn main() {
-    let mut args = std::env::args();
-    let _ = args.next().unwrap();
-    let server_addr = args.next().unwrap_or_else(|| "127.0.0.1:7890".to_owned());
-    let listener = std::net::TcpListener::bind(&server_addr).unwrap();
+    let server_addr = "127.0.0.1:7890";
+    let listener = TcpListener::bind(server_addr).unwrap();
     let mut smart_socket = SmartSocket::default();
     println!("Server listening on {}", server_addr);
+
     for connection in listener.incoming() {
-        let mut stream = match connection {
-            Ok(conn) => conn,
+        match connection {
+            Ok(mut stream) => {
+                let peer_addr = stream
+                    .peer_addr()
+                    .map(|a| a.to_string())
+                    .unwrap_or_else(|_| "unknown".into());
+                println!("Peer '{peer_addr}' connected");
+
+                let mut in_buffer = [0u8];
+                while stream.read_exact(&mut in_buffer).is_ok() {
+                    let response = smart_socket.process_command(in_buffer[0].into());
+                    let response_buf: [u8; 5] = response.into();
+                    if stream.write_all(&response_buf).is_err() {
+                        break;
+                    };
+                }
+
+                println!("Connection with {peer_addr} lost. Waiting for new connections...");
+            }
             Err(err) => {
                 println!("Can't receive connection: {}", err);
-                continue;
             }
-        };
-
-        let peer_addr = stream
-            .peer_addr()
-            .map(|a| a.to_string())
-            .unwrap_or_else(|_| "unknown".into());
-        println!("Peer '{}' connected", peer_addr);
-
-        let mut in_buffer = [0u8];
-        while stream.read_exact(&mut in_buffer).is_ok() {
-            let response = smart_socket.process_command(in_buffer[0].into());
-            let response_buf: [u8; 5] = response.into();
-            if stream.write_all(&response_buf).is_err() {
-                break;
-            };
         }
-
-        println!("Connection with {peer_addr} lost. Waiting for new connections...");
     }
 }
 
@@ -53,7 +54,7 @@ impl SmartSocket {
             }
             Command::Off => {
                 self.enabled = false;
-                Response::Ok
+                Response::Disabled
             }
             Command::IsOn => {
                 if self.enabled {
--- smart_socket/socket_lib/src/main.rs
+++ smart_socket/socket_lib/src/main.rs
@@ -1,29 +1,32 @@
-use std::{
-    error::Error,
-    io::{self, Write},
-};
+use std::io::{self, Write};
 
 use socket_lib::{Command, SmartSocketClient};
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let server_address = "127.0.0.1:8080";
-    let mut client = SmartSocketClient::new(server_address)?;
+fn main() {
+    let server_address = "127.0.0.1:7890";
+    let mut client = SmartSocketClient::new(server_address).unwrap();
 
     loop {
-        print!("Enter command (on/off/getvalue/ison): ");
-        io::stdout().flush()?;
+        print!("Enter command (on-1/off-2/getvalue-3/ison-4/exit): ");
+        io::stdout().flush().unwrap();
         let mut input = String::new();
-        io::stdin().read_line(&mut input)?;
+        io::stdin().read_line(&mut input).unwrap();
 
-        let command = match input.trim().to_lowercase().as_str() {
-            "on" => Command::On,
-            "off" => Command::Off,
-            "getvalue" => Command::GetValue,
-            "ison" => Command::IsOn,
-            _ => Command::Unknown,
+        let command = match input.trim() {
+            "1" => Some(Command::On),
+            "2" => Some(Command::Off),
+            "3" => Some(Command::GetValue),
+            "4" => Some(Command::IsOn),
+            _ => None,
         };
 
-        let response = client.run_command(command)?;
+        let response = match command {
+            Some(command) => client.run_command(command).unwrap(),
+            None => {
+                println!("Exitting...");
+                break;
+            }
+        };
         println!("Response: {}", response);
     }
 }
